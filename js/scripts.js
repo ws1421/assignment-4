7793e0df393dd9cd5d8b60ab29ccdff44f099599
ws1421 <78571620+ws1421@users.noreply.github.com>
2021-03-14 22:36:03 -0400

    add old astoria data, start styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,147 +1,215 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoiY3dob25nIiwiYSI6IjAyYzIwYTJjYTVhMzUxZTVkMzdmYTQ2YzBmMTM0ZDAyIn0.owNd_Qa7Sw2neNJbK6zc1A';
 
+// function to translate NYC land use codes into a color and a description
+var LandUseLookup = (code) => {
+  switch (code) {
+    case 1:
+      return {
+        color: '#f4f455',
+        description: '1 & 2 Family',
+      };
+    case 2:
+      return {
+        color: '#f7d496',
+        description: 'Multifamily Walk-up',
+      };
+    case 3:
+      return {
+        color: '#FF9900',
+        description: 'Multifamily Elevator',
+      };
+    case 4:
+      return {
+        color: '#f7cabf',
+        description: 'Mixed Res. & Commercial',
+      };
+    case 5:
+      return {
+        color: '#ea6661',
+        description: 'Commercial & Office',
+      };
+    case 6:
+      return {
+        color: '#d36ff4',
+        description: 'Industrial & Manufacturing',
+      };
+    case 7:
+      return {
+        color: '#dac0e8',
+        description: 'Transportation & Utility',
+      };
+    case 8:
+      return {
+        color: '#5CA2D1',
+        description: 'Public Facilities & Institutions',
+      };
+    case 9:
+      return {
+        color: '#8ece7c',
+        description: 'Open Space & Outdoor Recreation',
+      };
+    case 10:
+      return {
+        color: '#bab8b6',
+        description: 'Parking Facilities',
+      };
+    case 11:
+      return {
+        color: '#5f5f60',
+        description: 'Vacant Land',
+      };
+    case 12:
+      return {
+        color: '#5f5f60',
+        description: 'Other',
+      };
+    default:
+      return {
+        color: '#5f5f60',
+        description: 'Other',
+      };
+  }
+};
+
 var map = new mapboxgl.Map({
   container: 'mapContainer', // container ID
   style: 'mapbox://styles/mapbox/light-v10', // style URL
-  center: [-73.992313,40.678211], // starting position [lng, lat]
-  zoom: 9.3 // starting zoom
+  center: [-73.925,40.772], // starting position [lng, lat]
+  zoom: 14.8 // starting zoom
 });
 
-
-map.addControl(
-  new MapboxGeocoder({
-    accessToken: mapboxgl.accessToken,
-    mapboxgl: mapboxgl
-  })
-);
-
-
 map.on('style.load', function () {
+  // add a geojson source
+  map.addSource('old-astoria', {
+    type: 'geojson',
+    data: '/data/pluto.geojson'
+  });
 
-  // we need to load the data manually because our population properties are strings and not numbers!!!
-  $.getJSON('data/districts-with-population.geojson', function(featureCollection) {
+  // add a layer to style and display the source
+  map.addLayer({
+    'id': 'old-astoria-fill',
+    'type': 'fill',
+    'source': 'old-astoria',
+    'layout': {},
+    'paint': {
+      'fill-color': {
+        type: 'categorical',
+        property: 'landuse',
+        stops: [
+          [
+            '01',
+            LandUseLookup(1).color,
+          ],
+          [
+            '02',
+            LandUseLookup(2).color,
+          ],
+          [
+            '03',
+            LandUseLookup(3).color,
+          ],
+          [
+            '04',
+            LandUseLookup(4).color,
+          ],
+          [
+            '05',
+            LandUseLookup(5).color,
+          ],
+          [
+            '06',
+            LandUseLookup(6).color,
+          ],
+          [
+            '07',
+            LandUseLookup(7).color,
+          ],
+          [
+            '08',
+            LandUseLookup(8).color,
+          ],
+          [
+            '09',
+            LandUseLookup(9).color,
+          ],
+          [
+            '10',
+            LandUseLookup(10).color,
+          ],
+          [
+            '11',
+            LandUseLookup(11).color,
+          ],
 
-    // iterate over each feature in the FeatureCollection and convert the pop2010 property to a number
-    featureCollection.features.forEach(function(feature) {
-      feature.properties.pop2010 = parseInt(feature.properties.pop2010)
-    })
+        ]
+      },
+      'fill-outline-color': '#ccc',
+      'fill-opacity': 0.8
+    }
+  });
 
-    console.log(featureCollection)
-    console.log('hello')
-
-    console.log('style loaded')
-    // override the fill color of the water layer
-    map.setPaintProperty('water', 'fill-color', '#c9f4ff');
-
-    // add a geojson source
-    map.addSource('nyc-cd', {
-      type: 'geojson',
-      data: featureCollection
-    });
-
-    // add a layer to style and display the source
-    map.addLayer({
-      'id': 'nyc-cd',
-      'type': 'fill',
-      'source': 'nyc-cd',
-      'layout': {},
-      'paint': {
-        'fill-color': [
-          'interpolate',
-          ['linear'],
-          ['get', 'pop2010'],
-          0,
-          '#f1eef6',
-          50000,
-          '#bdc9e1',
-          100000,
-          '#74a9cf',
-          250000,
-          '#2b8cbe',
-          500000,
-          '#045a8d'
-        ],
-        'fill-outline-color': '#ccc',
-        'fill-opacity': 0.8
-      }
-    }, 'waterway-label');
-
-    // add an empty data source, which we will use to highlight the lot the user is hovering over
-    map.addSource('highlight-feature', {
-      type: 'geojson',
-      data: {
-        type: 'FeatureCollection',
-        features: []
-      }
-    })
-
-    // add a layer for the highlighted lot
-    map.addLayer({
-      id: 'highlight-line',
-      type: 'line',
-      source: 'highlight-feature',
-      paint: {
-        'line-width': 2,
-        'line-opacity': 0.9,
-        'line-color': 'white',
-      }
-    });
-
-
-    // listen for a click on the map and show info in the sidebar
-    map.on('click', function(e) {
-      // query for the features under the mouse, but only in the lots layer
-      var features = map.queryRenderedFeatures(e.point, {
-          layers: ['nyc-cd'],
-      });
-
-      if (features.length > 0 ) {
-        var hoveredFeature = features[0]
-        var cdName = hoveredFeature.properties.cd_name
-
-        var population_2010 = hoveredFeature.properties.pop2010
-
-        $('.cdname').text(cdName)
-        $('.population').text(numeral(population_2010).format('0.00a'))
-
-        // set this lot's polygon feature as the data for the highlight source
-        map.getSource('highlight-feature').setData(hoveredFeature.geometry);
-
-      }
-    })
-
-    // add a click listener for buttons in the sidebar.  On click, fly the map to a specific view
-    $('.flyButton').on('click', function() {
-      var cd = $(this).data('cd')
-
-      switch(cd) {
-        case 'bk-6':
-          map.flyTo({
-            center: [-73.979559, 40.676282],
-            zoom: 12
-          })
-
-          map.setLayoutProperty('nyc-cd', 'visibility', 'none');
-          break;
-        case 'mn-1':
-          // code block
-          break;
-        case 'bx-2':
-          // code block
-          break;
-        case 'reset':
-          map.flyTo({
-            center: [-73.992313,40.678211],
-            zoom: 9.3
-          })
-
-          map.setLayoutProperty('nyc-cd', 'visibility', 'visible');
-          break;
-        default:
-          // code block
-      }
-
-    })
+  // add an empty data source, which we will use to highlight the lot the user is hovering over
+  map.addSource('highlight-feature', {
+    type: 'geojson',
+    data: {
+      type: 'FeatureCollection',
+      features: []
+    }
   })
+
+  // add a layer for the highlighted lot
+  map.addLayer({
+    id: 'highlight-line',
+    type: 'line',
+    source: 'highlight-feature',
+    paint: {
+      'line-width': 2,
+      'line-opacity': 0.9,
+      'line-color': 'white',
+    }
+  });
+})
+
+// Create a popup, but don't add it to the map yet.
+var popup = new mapboxgl.Popup({
+  closeButton: false,
+  closeOnClick: false
+});
+
+map.on('mousemove', function (e) {
+  // query for the features under the mouse, but only in the lots layer
+  var features = map.queryRenderedFeatures(e.point, {
+      layers: ['old-astoria-fill'],
+  });
+
+  if (features.length > 0) {
+    // show the popup
+    // Populate the popup and set its coordinates
+    // based on the feature found.
+
+    var hoveredFeature = features[0]
+    var address = hoveredFeature.properties.address
+    var landuseDescription = LandUseLookup(parseInt(hoveredFeature.properties.landuse)).description
+
+    var popupContent = `
+      <div>
+        ${address}<br/>
+        ${landuseDescription}
+      </div>
+    `
+
+    popup.setLngLat(e.lngLat).setHTML(popupContent).addTo(map);
+
+    // set this lot's polygon feature as the data for the highlight source
+    map.getSource('highlight-feature').setData(hoveredFeature.geometry);
+
+    // show the cursor as a pointer
+    map.getCanvas().style.cursor = 'pointer';
+  } else {
+    // remove the Popup
+    popup.remove();
+
+    map.getCanvas().style.cursor = '';
+  }
 
 })
